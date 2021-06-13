de9aa935b5642c02fe1ef2c202f08dcf7e3b9096
ashirbad <ashirbadbehera29@gmail.com>
2021-06-14 00:50:53 +0530

    🌟 style Home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
+import { ReactComponent as CodingSVG } from '../../assets/coding.svg';
 import './Home.css';
 
 const Home = () => {
 	return (
 		<div className='home-container'>
-			<div className='header-text'>
-				<h1>Welcome to my portfolio!</h1>
-				<p>This is Ashirbad, A Web Developer</p>
+			<div className='home__left'>
+				<div className='header-text'>
+					<h1>
+						Hi There👋 <br /> I'm Ashirbad Behera
+					</h1>
+					<p>A Web Developer and student 👨‍💻</p>
+				</div>
+				<Nav />
 			</div>
-			<Nav />
+			<CodingSVG style={{ transform: 'scale(0.8)' }} className='home__svg' />
 		</div>
 	);
 };
