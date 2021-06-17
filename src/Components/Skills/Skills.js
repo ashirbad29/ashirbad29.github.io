0ba5b3dda928e2e26b80055ce3e1e3e83d3113b0
ashirbad <ashirbadbehera29@gmail.com>
2021-06-17 16:08:04 +0530

    📸 add image assets

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import FooterLink from '../FooterLink/FooterLink';
+import Nav from '../Nav/Nav';
+import { skillsList } from '../../assets/skillsData';
 import './Skills.css';
 
 const Skills = () => {
 	return (
-		<div>
-			<h1>I am Skills</h1>
+		<div className='section-container'>
+			<h1 className='center-text'>My Skills</h1>
+
+			<Nav />
+
+			<p>
+				Passionate about new technologies, I keep exploring stuffs, Here's the
+				tech stack i have worked with{' '}
+			</p>
+
+			<div className='skill-container'>
+				{skillsList.map((skill, idx) => {
+					return <img src={skill.iconUrl} alt='skill' key={idx} />;
+				})}
+			</div>
+
+			<FooterLink
+				phrase='Checkout my '
+				link='Projects!'
+				toAddress='/projects'
+			/>
 		</div>
 	);
 };
---
+++ src/assets/skillsData.js
@@ -0,0 +1,33 @@
+export const skillsList = [
+	{
+		skillName: 'HTML',
+		iconUrl: 'https://drive.google.com/uc?id=15lve9MrcSY0eXFZvHBgBD6xE7F4v3EAn',
+	},
+
+	{
+		skillName: 'CSS',
+		iconUrl: 'https://drive.google.com/uc?id=1XA_9BAdIAcsac9P0-XEnBNjICvml98y5',
+	},
+	{
+		skillName: 'JavaScript',
+		iconUrl: 'https://drive.google.com/uc?id=1h88ioQ-auloiD2aNXLffB8_oRXG8nBtf',
+	},
+
+	{
+		skillName: 'React',
+		iconUrl: 'https://drive.google.com/uc?id=1QaEMOQh0FF0EwHOmw7ecBs4gENLCyOwt',
+	},
+
+	{
+		skillName: 'Node',
+		iconUrl: 'https://drive.google.com/uc?id=1PhiY9m2iN53VtVzmJrxLd1MMX0klAI-8',
+	},
+	{
+		skillName: 'CPP',
+		iconUrl: 'https://drive.google.com/uc?id=1EC0gbLJve3CNp04D1yhjlE68VsFF9yUs',
+	},
+	{
+		skillName: 'Databases',
+		iconUrl: 'https://drive.google.com/uc?id=1pukCeF2K9TpMyV1CgjNCmjDzZwwgMHnA',
+	},
+];
