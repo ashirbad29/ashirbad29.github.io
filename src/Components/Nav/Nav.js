87b8e8bc94f8e10adbae2c0569b447ece2eb3932
ashirbad <ashirbadbehera29@gmail.com>
2021-06-17 17:37:26 +0530

    ⚡️ now hides same route button in navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Nav.css';
 const Nav = () => {
+	const { pathname } = useLocation();
+
 	return (
 		<nav>
 			<ul>
-				<li className='nav-links'>
+				<li className={`nav-links ${pathname === '/' ? 'hide' : ''}`}>
 					<Link className='link' to='/'>
 						Home
 					</Link>
 				</li>
-				<li className='nav-links'>
+				<li className={`nav-links ${pathname === '/about' ? 'hide' : ''}`}>
 					<Link className='link' to='/about'>
 						About
 					</Link>
 				</li>
-				<li className='nav-links'>
+				<li className={`nav-links ${pathname === '/contact' ? 'hide' : ''}`}>
 					<Link className='link' to='/contact'>
 						Contact
 					</Link>
 				</li>
-				<li className='nav-links'>
+				<li className={`nav-links ${pathname === '/projects' ? 'hide' : ''}`}>
 					<Link className='link' to='/projects'>
 						Projects
 					</Link>
 				</li>
-				<li className='nav-links'>
+				<li className={`nav-links ${pathname === '/skills' ? 'hide' : ''}`}>
 					<Link className='link' to='/skills'>
 						Skills
 					</Link>
